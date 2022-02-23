2f3f293a0d72b92cbb0e13bcf5a4b47424867330
Karri Wright <karriwright@Karris-MacBook-Pro.local>
2022-02-23 16:06:17 -0500

    add changes

--- src/com/company/WeatherReport.java
+++ src/com/company/WeatherReport.java
@@ -6,18 +6,34 @@ import java.util.Scanner;
 public class WeatherReport {
 
     public static void main(String[] args) {
-	Day myDay = new Day();
-    myDay.buildReport();
-    Scanner myScanner = new Scanner(System.in);
-    System.out.println("Please provide day of the week for your Weather Report");
-    String userInput = myScanner.nextLine();
-    myDay.DailyReport(userInput);
-    Random rand = new Random();
-    int upperbound = 6;
-    int number = rand.nextInt(upperbound);
-   System.out.println("The weather report for " + userInput + " will be "
-           +   myDay.buildFullReport.get(rand.nextInt(upperbound)));
+        Day myDay = new Day();
+        myDay.buildReport();
+        Scanner myScanner = new Scanner(System.in);
+        boolean playOn = false;
+        do {
 
+            System.out.println("Please provide day of the week for your Weather Report");
+            String userInput = myScanner.nextLine();
+            myDay.DailyReport(userInput);
+            Random rand = new Random();
+            int upperbound = 6;
+            int number = rand.nextInt(upperbound);
+            System.out.println("The weather report for " + userInput + " will be "
+                    + myDay.buildFullReport.get(rand.nextInt(upperbound)));
+            System.out.println("Do you want another Weather Report? Please type 1 for yes and type 2 for no");
+            int newInput = myScanner.nextInt();
 
+            if (newInput == 1) {
+                playOn = true;
+
+            } else if (newInput == 2) {
+                playOn = false;
+
+            }
+        } while (playOn);
     }
 }
+
+
+
+
