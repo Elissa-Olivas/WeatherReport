ff685bb969b87f72742da04170d189564c070361
Elissa Olivas <elissaolivas@Elissas-MacBook-Pro.local>
2022-02-24 13:48:05 -0700

    Updating loop to show correct info

--- src/com/company/WeatherReport.java
+++ src/com/company/WeatherReport.java
@@ -12,37 +12,36 @@ public class WeatherReport {
         boolean playOn = false;
         do {
             System.out.println("Please provide day of the week for your Weather Report");
-            String userInputDay = myScanner.nextLine();
+            String userInputDay = myScanner.next();
             myDay.dailyReport(userInputDay);
             Random rand = new Random();
             int upperbound = 6;
             int number = rand.nextInt(upperbound);
 
 
-//            if ((userInputDay == "Sunday") || (userInputDay == "Monday") || (userInputDay == "Tuesday")
-//                    || (userInputDay == "Wednesday") || (userInputDay == "Thursday") || (userInputDay == "Friday")
-//                    || (userInputDay == "Saturday")) {
-            //            System.out.println("The weather report for " + userInputDay + " will be "
-            //                    + myDay.buildFullReport.get(rand.nextInt(upperbound)));
-//            } else {
-//            System.out.println("That is an invalid option, please enter a day of the week.");
-//
+            if ((userInputDay.equals("Sunday")) || (userInputDay.equals("Monday")) || (userInputDay.equals("Tuesday"))
+                    || (userInputDay.equals("Wednesday")) || (userInputDay.equals("Thursday")) || (userInputDay.equals("Friday"))
+                    || (userInputDay.equals("Saturday"))) {
                 System.out.println("The weather report for " + userInputDay + " will be "
                         + myDay.buildFullReport.get(rand.nextInt(upperbound)));
-//            }
-            System.out.println("Do you want another Weather Report? Please type 1 for yes and type 2 for no");
-            int newInput = myScanner.nextInt();
-
-            if (newInput == 1) {
-                playOn = true;
-
-            } else if (newInput == 2) {
-                playOn = false;
-            System.out.println("Have a good day!");
+            } else {
+                System.out.println("That is an invalid option, please enter a day of the week.");
             }
-        } while (playOn);
+                System.out.println("Do you want another Weather Report? Please type 1 for yes and type 2 for no");
+                int newInput = myScanner.nextInt();
+
+                if (newInput == 1) {
+                    playOn = true;
+
+                } else if (newInput == 2) {
+                    playOn = false;
+                    System.out.println("Have a good day!");
+                }
+            }
+            while (playOn) ;
+        }
     }
-}
+
 
 
 
